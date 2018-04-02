c85a9c9e1bfe36cdacfc216f09ea0bfc9f11c955
Ali <atmacali_aa@hotmail.com>
2018-04-02 14:50:47 +0300

    materialview class sayfası eklendi

--- storageleaf/MaterialTableViewCell.swift
+++ storageleaf/MaterialTableViewCell.swift
@@ -9,5 +9,19 @@
 import UIKit
 
 class MaterialTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var materialImageView: UIImageView!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
 }
