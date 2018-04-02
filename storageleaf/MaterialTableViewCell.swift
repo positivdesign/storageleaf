b87c3eca86898c107b1ff250fcc1fb1224d04efc
Ali <atmacali_aa@hotmail.com>
2018-04-02 14:38:53 +0300

    table view eklendi

---
+++ storageleaf/MaterialTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MaterialTableViewCell.swift
+//  storageleaf
+//
+//  Created by Nergis Yıldız Atmaca on 2.04.2018.
+//  Copyright © 2018 AYDIN AYYILDIZ. All rights reserved.
+//
+
+import UIKit
+
+class MaterialTableViewCell: UITableViewCell {
+    
+}
